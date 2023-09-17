8f81cc46a076914092b556e9f44d9e6ce68cb621
Younisprogramming <younistaher12341@gmail.com>
2023-09-17 09:05:44 -0700

    [A[D[A[A[D

--- get_int.c
+++ get_int.c
@@ -3,13 +3,7 @@
  * get_int - awdawdaw
  * @num:dawa
  */
-void get_int(int num)
+int get_int(int num)
 {
-	int n;
-
-	if (num == 0)
-		return;
-	n = num / 10;
-	get_int(n);
-	putchr(num % 10 + '0');
+	return (num);
 }
