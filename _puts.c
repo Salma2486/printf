56c449372305a485de62cf0b541bc7ec8bda5157
Younisprogramming <younistaher12341@gmail.com>
2023-09-17 10:43:43 -0700

    test

--- _puts.c
+++ _puts.c
@@ -6,15 +6,16 @@
  */
 int _puts(char *str)
 {
-	int i = 0;
+	int i = 0, b = 0;
 
 	if (str)
 	{
 		while (str[i] != '\0')
 		{
 			putchar(str[i]);
+			b++;
 			i++;
 		}
 	}
-	return (strlen(str));
+	return (b);
 }
