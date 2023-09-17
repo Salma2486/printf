006c92a07d6b22df080cd73935ea254e1837cf80
Salma2486 <saloom_001@hotmail.com>
2023-09-17 09:26:07 -0700

    alrooob2

--- get_int.c
+++ get_int.c
@@ -1,29 +1,36 @@
 #include "main.h"
 /**
- * get_int - awdawdaw
- * @num:dawa
+ *get_int - awdawdaw
+ *@num: dawa
+ *Return: suhdfno
  */
 int get_int(int num)
 {
 	char buffer[12];
 	int index = 0;
 
-	if (number == 0) {
+	if (number == 0)
+	{
 		buffer[index++] = '0';
-	} else {
-
-		if (number < 0) {
+	}
+	else
+	{
+		if (number < 0)
+		{
 			buffer[index++] = '-';
 			number = -number;
 		}
-		while (number != 0) {
-			buffer[index++] = '0' + (number % 10); // Convert digit to character
+		while (number != 0)
+		{
+			buffer[index++] = '0' + (number % 10);
 			number /= 10;
 		}
 	}
 	buffer[index] = '\0';
-	for (int start = 0, end = index - 1; start < end; start++, end--) {
+	for (int start = 0, end = index - 1; start < end; start++, end--)
+	{
 		char temp = buffer[start];
+
 		buffer[start] = buffer[end];
 		buffer[end] = temp;
 	}
