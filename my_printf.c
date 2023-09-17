00818a821772f749f133e6051c4d5233b7fc71b1
Younisprogramming <younistaher12341@gmail.com>
2023-09-17 07:41:59 -0700

    test

--- my_printf.c
+++ my_printf.c
@@ -7,34 +7,36 @@
 int _printf(const char *format, ...)
 {
 	unsigned int h = 0, r_value = 0;
-	int r_val;
-	char *c;
 	va_list args;
 
 	va_start(args, format);
+
 	for ( ; format[h] != '\0' ; h++)
 	{
 		if (format[h] != '%')
 		{
-			write(1, &format[h], 1);
+			putchr(format[h]);
 		}
 		else if (format[h + 1] == 'c')
 		{
-			c = va_arg(args, char *);
-			write(1, &c, 1);
+			putchr(va_arg(args, int));
 			h++;
 		}
 		else if (format[h + 1] == 's')
 		{
-			c = va_arg(args, char *);
-			write(1, c, strlen(c));
-			r_val = strlen(c);
+			int r_val = put_s(va_arg(args, char *));
+
 			h++;
-			r_value += r_val;
+			r_value += (r_val - 1);
 		}
 		else if (format[h + 1] == '%')
 		{
-			write(1, "%", 1);
+			putchr('%');
+			h++;
+		}
+		else if ((format[h + 1] == 'd') || (format[h + 1] == 'i'))
+		{
+			get_int(va_arg(args, int));
 			h++;
 		}
 		r_value += 1;
