4a49aaf8e0b2ff24f333be9b4b9e8d91fa6bc36c
Salma2486 <saloom_001@hotmail.com>
2023-09-17 04:18:10 -0700

    task0

--- _printf.c
+++ _printf.c
@@ -9,6 +9,7 @@ int _printf(const char *format, ...)
 	char *ch;
 	char *str;
 	va_list args;
+	int str_len = 0;
 
 	if (format == NULL)
 		return (-1);
@@ -25,8 +26,10 @@ int _printf(const char *format, ...)
 				format++; }
 			else if (*format == 's')
 			{ str = va_arg(args, char *);
-				write(1, str, strlen(str));
-				format++; }
+				while (str[str_len] != '\0')
+					str_len++;
+				write(1, str, str_len);
+				format += str_len; }
 			else if (*format == '%')
 			{ write(1, format, 1);
 				format++; }
