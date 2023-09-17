6dbafdb96ea17fb9f984af0457538f0fd990594d
Salma2486 <saloom_001@hotmail.com>
2023-09-17 03:24:45 -0700

    task0

---
+++ _printf.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ *_printf - This is the entry point of the code
+ *@format: jnfjw
+ *Return:0 Success
+ */
+int _printf(const char *format, ...)
+{
+	char *ch;
+	char *str;
+	va_list args;
+
+	if (format == NULL)
+		return (-1);
+	va_start(args, format);
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{ format++;
+			if (*format == 'c')
+			{ ch = va_arg(args, char *);
+				write(1, &ch, 1);
+				format++; }
+			else if (*format == 's')
+			{ str = va_arg(args, char *);
+				write(1, str, strlen(str));
+				format++; }
+			else if (*format == '%')
+			{ write(1, format, 1);
+				format++; }
+		}
+		else
+		{ write(1, format, 1);
+			format++; }
+	}
+	va_end(args);
+	return (0);
+}
---
+++ main.c
@@ -0,0 +1,9 @@
+#include "main.h"
+int main(void)
+{
+_printf("String:[%s]\n", "I am a string !");
+_printf("Percent:[%%]\n");
+_printf("Character:[%c]\n", 'H');
+_printf(NULL);
+return (0);
+}
