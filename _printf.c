cc76d52e5651247e33ff39e3beaab5eb25fb8404
Younisprogramming <younistaher12341@gmail.com>
2023-09-18 10:41:42 -0700

    oooooooo

--- _printf.c
+++ _printf.c
@@ -11,7 +11,7 @@ int _printf(const char *format, ...)
 	va_list args;
 
 	va_start(args, format);
-	if (!format || (format[h] == '%' && !format[h + 1]))
+	if (!format)
 		return (-1);
 	if (!format[h])
 		return (-1);
