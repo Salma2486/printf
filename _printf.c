e2d000ad07cbdd6674241e5e30882d31f2d21afe
Younisprogramming <younistaher12341@gmail.com>
2023-09-18 11:36:09 -0700

    2

--- _printf.c
+++ _printf.c
@@ -38,8 +38,8 @@ int _printf(const char *format, ...)
 		{
 			putchr('%');
 			h++; }
-		else
-			putchr('%');
+		else if (!format[h + 1] || format[h + 1] == ' ')
+			return (-1);
 		r_value += 1;
 	}
 	va_end(args);
