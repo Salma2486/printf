8855ba59021cbf3b50002ab9a030036a32659097
Salma2486 <saloom_001@hotmail.com>
2023-09-19 08:36:47 -0700

    task2

--- printBinary.c
+++ printBinary.c
@@ -1,36 +1,39 @@
 #include "main.h"
 /**
  *printBinary - This is the entry point of the code
- *@num: oudfhnoi
+ *@num:l kjfvnk
  *Return:0 Success
  */
 int printBinary(unsigned int num)
 {
-	char buffer[BUFF_SIZE];
+	char buffer[BUFF_SIZE], temp;
 	int index = 0;
-	int i;
+	int i, digit;
 
 	if (num == 0)
 	{
-		putchr('0');
+		buffer[index++] = '0';
 	}
 	else
 	{
 		while (num > 0)
 		{
-			int digit = num % 2;
-
+			digit = num % 2;
 			buffer[index++] = '0' + digit;
 			num /= 2;
 		}
-		for (i = 0; i < index / 2; i++)
-		{
-			char temp = buffer[i];
-
-			buffer[i] = buffer[index - 1 - i];
-			buffer[index - 1 - i] = temp;
-		}
 	}
+	if (index == 0)
+	{
+		buffer[index++] = '0';
+	}
+	for (i = 0; i < index / 2; i++)
+	{
+		temp = buffer[i];
+		buffer[i] = buffer[index - 1 - i];
+		buffer[index - 1 - i] = temp;
+	}
+
 	buffer[index] = '\0';
 	return (_puts(buffer));
 }
